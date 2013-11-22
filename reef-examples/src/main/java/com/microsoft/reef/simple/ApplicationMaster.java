0436614654ab5b8784e043258fdc6c2006d796e4
Rusty Sears <sears@microsoft.com>
2013-11-21 17:16:59 -0800

    Stubbed out APIs.

--- reef-examples/src/main/java/com/microsoft/reef/simple/ApplicationMaster.java
+++ reef-examples/src/main/java/com/microsoft/reef/simple/ApplicationMaster.java
@@ -1,6 +1,14 @@
 package com.microsoft.reef.simple;
 
 import java.io.PrintStream;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Logger;
 
 import com.microsoft.reef.driver.activity.CompletedActivity;
@@ -48,5 +56,57 @@ public abstract class ApplicationMaster {
   public void onContainerFailed(FailedEvaluator context) { }
   public void onContainerStopped(ClosedContext context)  { }
   public void onContainerMessage(ContextMessage context) { }
+  
+  // ExecutorService-style calls.
+  
+  public void execute(AsyncTaskRequest arg0) {
+    queueTaskForExecution(arg0);
+  }
+  public boolean awaitTermination(long timeout, TimeUnit unit)
+      throws InterruptedException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
+      throws InterruptedException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks,
+      long timeout, TimeUnit unit) throws InterruptedException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks)
+      throws InterruptedException, ExecutionException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout,
+      TimeUnit unit) throws InterruptedException, ExecutionException,
+      TimeoutException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  public boolean isShutdown() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+  public boolean isTerminated() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+  public void shutdown() {
+    // TODO Auto-generated method stub
+    
+  }
+  public List<Runnable> shutdownNow() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  public <T> Future<T> submit(AsyncTaskRequest task) {
+    // TODO Auto-generated method stub
+    return null;
+  }
   
 }
